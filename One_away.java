3146b4db082ca0b2825bc2e1cdb0138ce1055fd1
Sathyaram <32465233+sathyaram5@users.noreply.github.com>
2018-10-22 23:08:33 -0400

    Update One_away.java

--- One_away.java
+++ One_away.java
@@ -1,44 +1,44 @@
 boolean oneEditAway(String first, String second) {
- if (first.length()== second.length()) {
-return oneEditReplace(first, second);
- } else if (first.length()+ 1 == second.length()) {
- return oneEditinsert(first, second);
+ if (first.length() == second.length()) {
+  return oneEditReplace(first, second);
+ } else if (first.length() + 1 == second.length()) {
+  return oneEditinsert(first, second);
  } else if (first.length() - 1 == second.length()) {
- return oneEditinsert(second, first);
+  return oneEditinsert(second, first);
  }
  return false;
- }
+}
 
 boolean oneEditReplace(String sl, String s2) {
  boolean foundDifference = false;
- for (int i= 0; i < sl.length(); i++) {
- if (sl.charAt(i) != s2.charAt(i)) {
- if (foundDifference) {
+ for (int i = 0; i < sl.length(); i++) {
+  if (sl.charAt(i) != s2.charAt(i)) {
+   if (foundDifference) {
 
-}
-return false;
-}
+   }
+   return false;
+  }
 
-foundDifference = true;
-}
+  foundDifference = true;
  }
- return true;
- }
+}
+return true;
+}
 
- /* Check if you can insert a character into sl to make s2. */
- boolean oneEditinsert(String sl, String s2) {
+/* Check if you can insert a character into sl to make s2. */
+boolean oneEditinsert(String sl, String s2) {
  int indexl = 0;
  int index2 = 0;
  while (index2 < s2.length() && indexl < sl.length()) {
- if (sl.charAt(indexl) != s2.charAt(index2)) {
- if (indexl != index2) {
- return false;
- }
- index2++;
- } else {
- indexl++;
- index2++;
- }
+  if (sl.charAt(indexl) != s2.charAt(index2)) {
+   if (indexl != index2) {
+    return false;
+   }
+   index2++;
+  } else {
+   indexl++;
+   index2++;
+  }
  }
  return true;
- }
+}
