daba68a018c1d83dbbc25b31d27ff4ba61c060f6
Sathyaram <32465233+sathyaram5@users.noreply.github.com>
2018-10-22 23:11:00 -0400

    Update checkif_permutations.java

--- checkif_permutations.java
+++ checkif_permutations.java
@@ -1,18 +1,20 @@
 boolean permutation(String s, String t) {
  if (s.length() != t.length()) {
- return false;
+  return false;
  }
 }
-int[] letters = new int[128]; II Assumption
+int[] letters = new int[128];
+II Assumption
 char[] s_array = s.toCharArray();
-for (char c : s_array) { II count number of each char in s.
-letters[c]++;
+for (char c: s_array) {
+ II count number of each char in s.
+ letters[c]++;
 }
-for (int i= 0; i < t.length(); i++) {
-int c = (int) t.charAt(i);
-letters[c]--;
-if (letters[c] < 0) {
-return false;
-}
+for (int i = 0; i < t.length(); i++) {
+ int c = (int) t.charAt(i);
+ letters[c]--;
+ if (letters[c] < 0) {
+  return false;
+ }
 }
 return true;
