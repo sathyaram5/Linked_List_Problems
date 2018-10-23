e1e3f9e90d7a53735be71bfcb50abd74b37a049b
Sathyaram <32465233+sathyaram5@users.noreply.github.com>
2018-10-22 23:10:02 -0400

    Update Palindrome_Permutation.java

--- Palindrome_Permutation.java
+++ Palindrome_Permutation.java
@@ -1,43 +1,42 @@
 boolean isPermutationOfPalindrome(String phrase) {
  int[] table = buildCharFrequencyTable(phrase);
  return checkMaxOneOdd(table);
- }
+}
 
 /* Check that no more than one character has an odd count. */
- boolean checkMaxOneOdd(int[] table) {
+boolean checkMaxOneOdd(int[] table) {
  boolean foundOdd = false;
-for (int count : table) {
-if (count% 2 == 1) {
-if (foundOdd) {
- return false;
+ for (int count: table) {
+  if (count % 2 == 1) {
+   if (foundOdd) {
+    return false;
+   }
+   foundOdd = true;
+  }
  }
- foundOdd = true;
- }
-}
  return true;
 }
 
- /* Map each character to a number. a -> 0, b -> 1, c -> 2, etc.
+/* Map each character to a number. a -> 0, b -> 1, c -> 2, etc.
  * This is case insensitive. Non-letter characters map to -1. */
 int getCharNumber(Character c) {
-int a= Character.getNumericValue('a');
-int z = Character.getNumericValue('z');
-int val= Character.getNumericValue(c);
- if (a<= val && val<= z) {
-return val - a;
-}
+ int a = Character.getNumericValue('a');
+ int z = Character.getNumericValue('z');
+ int val = Character.getNumericValue(c);
+ if (a <= val && val <= z) {
+  return val - a;
+ }
  return -1;
 }
 
 /* Count how many times each character appears. */
 int[] buildCharFrequencyTable(String phrase) {
-int[] table new int[Character.getNumericValue('z')
- Character.getNumericValue('a') + 1];
-for (char c phrase.toCharArray()) {
- int x = getCharNumber(c);
-if (x != -1) {
-table[x]++;
-}
-}
-return table;
+ int[] table new int[Character.getNumericValue('z') Character.getNumericValue('a') + 1];
+ for (char c phrase.toCharArray()) {
+  int x = getCharNumber(c);
+  if (x != -1) {
+   table[x]++;
+  }
+ }
+ return table;
 }
